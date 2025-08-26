8e80cb4b178c5b31eb2f47fc5d05d336d27b34b5
muh daniel al hari <muhdaniel23az@gmail.com>
2025-08-26 11:10:42 +0100

    komitan1

--- luas_segitiga.php
+++ luas_segitiga.php
@@ -1,5 +1,10 @@
 <?php
+function luasSegitiga($alas, $tinggi) {
+    return 0.5 * $alas * $tinggi;
+}
+
 $alas = 8;
 $tinggi = 5;
+
 echo "Luas Segitiga dengan alas $alas dan tinggi $tinggi = " . luasSegitiga($alas, $tinggi);
 ?>
